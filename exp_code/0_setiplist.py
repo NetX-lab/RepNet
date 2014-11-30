7a01bf4ccf1589102b2896358f5bf4c2639bee5e
iQua <iqua@miami.csl.utoronto.ca>
2014-11-29 23:23:18 -0500

    update iplist setup method

--- exp_code/0_setiplist.py
+++ exp_code/0_setiplist.py
@@ -8,30 +8,30 @@ numB = int(serverB.readline())
 iplistA = open('iplistA', 'w')
 iplistB = open('iplistB', 'w')
 
-sshconfig = open('/home/lsh/.ssh/config', 'w')
-csshconfig = open('/home/lsh/.clusterssh/clusters', 'w')
+sshconfig = open('/Users/iqua/.ssh/config', 'w')
+csshconfig = open('/etc/clusters', 'w')
 
 csshconfig.write("rackA ")
 for i in range(numA):
   port = int(serverA.readline())
-  #content = "host A" + str(port) + "\n  HostName 192.168.1." + str(port - 30000) + "\n  IdentityFile /home/lsh/.ssh/id_rsa\n"
-  content = "host A" + str(port) + "\n  HostName 192.168.1." + str(port - 30000) + "\n  ProxyCommand ssh shuhao@sing.cse.ust.hk -W %h:22" + "\n  IdentityFile /home/lsh/.ssh/id_rsa\n"
+  content = "host A" + str(port) + "\n  Hostname sing.cse.ust.hk\n  User shuhao\n  Port " + str(port) + "\n"
   sshconfig.write(content)
   hostname = "A" + str(port) + " "
   csshconfig.write(hostname)
-  ipaddr = "192.168.6." + str(port - 30100) + "\n"
+  ipaddr = "192.168.6." + str(port - 30042) + "\n"
   iplistA.write(ipaddr)
 iplistA.close()
 
 csshconfig.write("\nrackB ")
 for i in range(numB):
   port = int(serverB.readline())
-  #content = "host B" + str(port) + "\n  HostName 192.168.1." + str(port - 30000) + "\n  IdentityFile /home/lsh/.ssh/id_rsa\n"
-  content = "host B" + str(port) + "\n  HostName 192.168.1." + str(port - 30000) + "\n  ProxyCommand ssh shuhao@sing.cse.ust.hk -W %h:22" + "\n  IdentityFile /home/lsh/.ssh/id_rsa\n"
+  content = "host B" + str(port) + "\n  Hostname sing.cse.ust.hk\n  User shuhao\n  Port " + str(port) + "\n"
   sshconfig.write(content)
   hostname = "B" + str(port) + " "
   csshconfig.write(hostname)
-  ipaddr = "192.168.7." + str(port - 30106) + "\n"
+  if (port == 30055):
+    port = 30050
+  ipaddr = "192.168.7." + str(port - 30048) + "\n"
   iplistB.write(ipaddr)
 iplistB.close()
 
@@ -40,18 +40,19 @@ csshconfig.close()
 serverA.close()
 serverB.close()
 
-csshconfig = open('/home/lsh/.clusterssh/clusters', 'r')
+csshconfig = open('/etc/clusters', 'r')
 serverA = csshconfig.readline().split()
 serverB = csshconfig.readline().split()
 csshconfig.close()
 
 for i in range(1, numA+1):
   os.system("echo '" + str(i) + "' > iplist  && cat iplistB >> iplist")
-  os.system("scp iplist shuhao@" + serverA[i] + ":~/repflowtestbed/iplist")
-  print "Done copying iplist to ", serverA[i]
+  os.system("scp ./iplist " + serverA[i] + ":~/repnet/exp_code/iplist")
+  print "Done copying iplist to", serverA[i]
 for j in range(1, numB+1):
   os.system("echo '" + str(i+j) + "' > iplist  && cat iplistA >> iplist")
-  os.system("scp iplist shuhao@" + serverB[j] + ":~/repflowtestbed/iplist")
-  print "Done copying iplist to ", serverB[j]
+  cmd = "scp ./iplist " + serverB[j] + ":~/repnet/exp_code/iplist"
+  os.system(cmd)
+  print "Done copying iplist to", serverB[j]
 
 os.system("rm iplist*")
